c18ac08da3b2a1970089d7faa7bb6aa3e8a538cf
Silvio Almeida <silviotmalmeida@gmail.com>
2024-09-27 08:08:25 -0300

    Ajustes gerais

--- updateLinux.sh
+++ updateLinux.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-
+# apt
 sudo apt update --fix-missing
-
 sleep 1
-
 sudo apt upgrade -y
-
 sleep 1
-
 sudo apt autoremove -y
-
 sleep 1
-
 sudo apt autoclean -y
 
 sleep 1
 
+# flatpak
 sudo flatpak update -y
+sleep 1
+sudo flatpak repair
+sleep 1
+sudo flatpak update -y
+sleep 1
+sudo flatpak uninstall --unused
 
 sleep 1
 
+# snap
 sudo snap refresh -y
